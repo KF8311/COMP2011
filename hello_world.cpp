4b595065199d2a2c741a23eddc17f9cb5f1067c5
phlamah <phlamah@connect.ust.hk>
2025-02-04 04:42:29 +0000

    testing

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World\n";
+    char money[] = "Bye World";
+    std::cout << "Test\n";
+    std::cout << money << "\n";
+    return 0;
+}
