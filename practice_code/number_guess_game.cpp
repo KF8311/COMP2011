c2c0e6a45172ccdf9787771154ff2d930e53bf45
phlamah <phlamah@connect.ust.hk>
2025-02-06 08:48:19 +0000

    number guessing practice

---
+++ practice_code/number_guess_game.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+    int number;
+    const int MAX_RANGE = 100;
+    int guessed = 0;
+    int turn = 1;
+    number = rand() % MAX_RANGE + 1;
+    //cout << number << endl;
+
+    int guessing_number = 0;
+    while (guessed == 0)
+    {
+        cout << "Player " << turn << ", please enter your guess:\n";
+        cin >> guessing_number;
+        if (guessing_number > number)
+        {
+            cout << "Sorry, the number is smaller than " << guessing_number << endl;
+        }
+        else if (guessing_number < number)
+        {
+            cout << "Sorry, the number is larger than " << guessing_number << endl;
+        }
+        else
+        {
+            cout << "Player " << turn << ", you win!!!" << endl;
+            break;
+        }
+        if (turn == 1)
+        {
+            turn = 2;
+        }
+        else if (turn == 2)
+        {
+            turn = 1;
+        }
+    }
+    return 0;
+}
