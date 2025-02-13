4e1383b54a5ac2a179e5dd6573d55d229105ce0d
phlamah <phlamah@connect.ust.hk>
2025-02-13 08:53:27 +0000

    small fix

--- practice_code/RAT.cpp
+++ practice_code/RAT.cpp
@@ -1,27 +1,27 @@
-//RAT.cpp
+// RAT.cpp
 #include <iostream>
 using namespace std;
 const int SCREEN_SIZE = 110;
 
 void multi_rat(int height, int repeat)
 {
-    for (int j = 0; j < height; j++)//loop through column
+    for (int j = 0; j < height; j++) // loop through column
     {
-        for (int i = 0; i < height * repeat; i++) //loop through row
+        for (int i = 0; i < height * repeat; i++) // loop through row
         {
-            (i % height <= j) ? cout << '*' : cout << ' '; //is it the desired place?, the like is it the first of of the triangle, 1,2,3...
+            (i % height <= j) ? cout << '*' : cout << ' '; // is it the desired place?, the like is it the first of of the triangle, 1,2,3...
         }
-        cout << endl; //finish a row, go to next row
+        cout << endl; // finish a row, go to next row
     }
 }
 int main()
 {
     int height;
-    //int repeat;
+    // int repeat;
     cout << "Height?\n";
     cin >> height;
-    //cout << "Repeat how many times?\n";
-    //cin >> repeat;
+    // cout << "Repeat how many times?\n";
+    // cin >> repeat;
     multi_rat(height, SCREEN_SIZE / height);
     return 0;
 }
